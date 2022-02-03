457f6b18fa8a914a166b8e033a41fc935650d8e2
Quirk23 <rehan.qureshi@carbonteq.com>
2022-02-03 13:40:00 +0000

    Added a rounded rectangle.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { useAspect } from "@react-three/drei";
+import { useAspect, RoundedBox, Text, Billboard, Environment } from "@react-three/drei";
 import { Intro } from "./Intro";
-
+import * as THREE from 'three'
 import { Flex, Box } from "@react-three/flex";
 
 const Layout = () => (
@@ -17,15 +17,70 @@ const Layout = () => (
 );
 
 const Grid = () => (
-    <Flex justifyContent="center" alignItems="center" flexDirection="Column">
+  <Flex justifyContent="center" alignItems="center" flexDirection="Column">
     <Box centerAnchor>
       <Layout />
     </Box>
     <Box centerAnchor flexGrow={1} marginTop={20}>
       <Layout />
     </Box>
+    <Box centerAnchor marginTop={20}>
+      <Suspense>
+        <RoundedBox args={[200, 80, 0]} radius={5} smoothness={4}>
+          <meshBasicMaterial color="royalblue" />
+        </RoundedBox>
+        <Text color="#f3f3f3" anchorX="center" anchorY="middle" fontSize={25}>
+          Vodcast
+        </Text>
+      </Suspense>
+    </Box>
   </Flex>
 );
+
+function setupVideo() {
+  const video = document.createElement('video');
+
+  var playing = false;
+  var timeupdate = false;
+
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      /* use the stream */
+      video.srcObject = stream;
+    })
+    .catch(function (err) {
+      /* handle the error */
+    });
+
+  video.autoplay = true;
+  video.muted = true;
+  video.loop = true;
+
+  // Waiting for these 2 events ensures
+  // there is data in the video
+
+  video.addEventListener('playing', function () {
+    playing = true;
+    checkReady();
+  }, true);
+
+  video.addEventListener('timeupdate', function () {
+    timeupdate = true;
+    checkReady();
+  }, true);
+
+  // video.src = url;
+  video.play();
+
+
+  function checkReady() {
+    if (playing && timeupdate) {
+      copyVideo = true;
+    }
+  }
+
+  return video;
+}
 
 function Scene() {
   const size = useAspect(1280, 720, 0.3);
